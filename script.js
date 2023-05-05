81d4e29f5e1271aceb4e35152ed32942c583106d
Victor Oviedo <victor.oviedo.a87@gmail.com>
2023-05-05 09:53:15 -0300

    dando funcionalidad con Js

---
+++ script.js
@@ -0,0 +1,44 @@
+function encriptar() {
+    var texto = document.getElementById("textarea").value.toLowerCase();
+
+    var textCifrado = texto.replace(/e/igm,"enter");
+    var textCifrado = textCifrado.replace(/o/igm,"ober");
+    var textCifrado = textCifrado.replace(/i/igm,"imes");
+    var textCifrado = textCifrado.replace(/a/igm,"ai");
+    var textCifrado = textCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("robot").style.display = "none";
+    document.getElementById("result").innerHTML = textCifrado;
+    document.getElementById("robot2").style.display = "show";
+    document.getElementById("robot2").style.display = "flex";
+    document.getElementById("copy").style.display = "show";
+    document.getElementById("copy").style.display = "flex";
+    document.getElementById("textarea").value = "";
+
+}
+
+function desencriptar() {
+    var texto = document.getElementById("textarea").value.toLowerCase();
+
+    var textCifrado = texto.replace(/enter/igm,"e");
+    var textCifrado = textCifrado.replace(/ober/igm,"o");
+    var textCifrado = textCifrado.replace(/imes/igm,"i");
+    var textCifrado = textCifrado.replace(/ai/igm,"a");
+    var textCifrado = textCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("robot").style.display = "none";
+    document.getElementById("result").innerHTML = textCifrado;
+    document.getElementById("copy").style.display = "show";
+    document.getElementById("copy").style.display = "flex";
+    document.getElementById("textarea").value = "";
+
+}
+
+function copiar() {
+    var contenido = document.getElementById("result").textContent;
+    console.log({contenido});
+    navigator.clipboard.writeText(contenido);
+    alert("Se copio");
+    document.getElementById("textarea").value = "";
+    
+}
